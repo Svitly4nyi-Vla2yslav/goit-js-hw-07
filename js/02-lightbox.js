0df7b26b0380bdad91dc2d7adb6814820cbe2bcc
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-07-11 22:44:38 +0200

    www

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
 console.log(galleryItems);
